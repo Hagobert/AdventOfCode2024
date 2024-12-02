c5c508ed07f28066002cf0d968207664bd997528
hagenkellermann <hagen.kellermann@tngtech.com>
2024-12-02 11:07:53 +0100

    solve problems of day 2

--- src/main/kotlin/day2/problems.kt
+++ src/main/kotlin/day2/problems.kt
@@ -20,11 +20,12 @@ fun analyzeLevelReports(levelReports: List<List<Int>>, problemDamperActive: Bool
 
 fun analyzeLevelReport(levelReport: List<Int>): LevelReportResult {
     val initialDifference = levelReport[1] - levelReport[0]
+    val initialSlope = initialDifference > 0
     if (initialDifference !in -3..3 || initialDifference == 0) return LevelReportResult(false, setOf(0, 1))
 
     levelReport.drop(2).forEachIndexed { i, value ->
-        val diff = value - levelReport[i + 1]
-        if (diff !in -3..3 || diff == 0 || (diff > 0) != (initialDifference > 0)) {
+        val difference = value - levelReport[i + 1]
+        if (difference !in -3..3 || difference == 0 || (difference > 0) != initialSlope) {
             // Normally, only the previous index (i+1) has to be considered for the problem damper. Only if i==0, index 0 is relevant as well since it sets the initial slope
             return LevelReportResult(false, if (i == 0) setOf(0, 1, 2) else setOf(i + 1, i + 2))
         }
